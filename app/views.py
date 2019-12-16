cb33d7d56776ccac0ac64a2a454d28aa47e9127b
Marat <lmar@mail.ru>
2019-12-16 18:27:04 +0300

    something strange with append to session variable

--- app/views.py
+++ app/views.py
@@ -19,16 +19,31 @@ def product_list_view(request):
 def product_view(request, pk):
     template = 'app/product_detail.html'
     product = get_object_or_404(Product, id=pk)
-
+    session = request.session.session_key
+    print('Session id:', session)
     form = ReviewForm(request.POST)
     #print('Form type:', type(form), 'Form content:', form)
+    if 'reviewed_products' in request.session.keys():
+        reviewed = request.session.get('reviewed_products')
+        print('Reviewed items:', reviewed)
+    else:
+        request.session['reviewed_products'] = []
+    print('req session keys:', request.session.keys())
+
     if request.method == 'POST':
         #if form.is_valid():
-        review = Review()
-        review.text = form.cleaned_data['text']
-        review.product = product
-        # логика для добавления отзыва
-        review.save()
+        if pk not in reviewed:
+            print('Product id:', pk, ', ... you are in not_revieved selection')
+            review = Review()
+            #review.text = form.cleaned_data['text']
+            review.product = product
+            # логика для добавления отзыва
+            review.save()
+            request.session['reviewed_products'].append(pk)
+            print('Reviewed items:', request.session.get('reviewed_products'))
+        else:
+            print('you review to this product allways exist')
+
     reviews = Review.objects.all()
     for k in reviews:
         print(k.text)
